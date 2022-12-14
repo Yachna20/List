c5033ff34889700c5ea7f048d8e96ebc05bdb749
Yachna20 <thakreyachna20@gmail.com>
2022-12-14 16:29:45 +0530

    [Yachna]-Add Nodes

---
+++ LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Linkedlist list = new Linkedlist();
+            list.Addnode(10);
+            list.Addnode(20);
+            list.Addnode(30);
+            list.Display();
+        }
+    }
+}
